33df813b89441a79ced917fef860e5df7cb4eb83
Pranav Dalvi <pranav.dalvi@zohomail.in>
2026-09-22 15:50:12 +0530

    fix(migrations): linearize migration head by setting down_revision to e7f8a9b0c1d2

--- migrations/versions/x1y2z3a4b5c6_add_custom_storage_to_groups.py
+++ migrations/versions/x1y2z3a4b5c6_add_custom_storage_to_groups.py
@@ -9,7 +9,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "x1y2z3a4b5c6"
-down_revision = "w7x8y9z0a1b2"
+down_revision = "e7f8a9b0c1d2"
 branch_labels = None
 depends_on = None
 
